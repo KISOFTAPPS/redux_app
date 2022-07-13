02e25708f7c62bb1c8e989cd0ee0344c45fa36b6
KISOFTAPPS <kisoftapps@outlook.com>
2022-07-13 11:40:59 -0700

    Primera practica terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./store/slices/counter";
 
 const App = () => {
-  return (
-    <div>
-      HOLA
-    </div>
-  )
-}
+    const { counter } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
 
-export default App
+    const incrementBy = 10
+
+    return (
+        <div>
+          <h1>Count is: {counter}</h1>
+            <button type="button" onClick={() => dispatch(increment())}>
+                Increment
+            </button>
+            <button type="button" onClick={() => dispatch(decrement())}>
+                Dercrement
+            </button>
+            <button type="button" onClick={() => dispatch(incrementByAmount(incrementBy))}>
+            Increment by {incrementBy}
+            </button>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/PokemonApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonApp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PokemonApp
